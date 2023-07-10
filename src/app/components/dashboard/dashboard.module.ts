2dd446c6e2f5f967595a7240cec8d94e38b90eb9
ignacio quezada <ig.quezad>
2023-07-10 19:20:11 -0400

    Se añadio 3 componentes el inicio,navbar,repoertes

--- src/app/components/dashboard/dashboard.module.ts
+++ src/app/components/dashboard/dashboard.module.ts
@@ -3,10 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { InicioComponent } from './inicio/inicio.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { ReportesComponent } from './reportes/reportes.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    InicioComponent,
+    NavbarComponent,
+    UsuariosComponent,
+    ReportesComponent
+  ],
   imports: [
     CommonModule,
     DashboardRoutingModule,
